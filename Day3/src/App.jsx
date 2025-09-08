0861126db99e4c45ecfb87fb1a89026b86336da0
Ashish <ay0393101@gmail.com>
2025-09-08 20:21:14 +0530

    Created new components CopyInput and PoopupContent

---
+++ Day3/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CopyInput from "./components/CopyInput";
+import Switcher from "./components/Switcher";
+
+const App = () => {
+  return (
+    <div>
+      {/* <CopyInput /> */}
+      <Switcher />
+    </div>
+  );
+};
+
+export default App;
